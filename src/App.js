1f6373e5b5802925f5f81619cdcc6375c68fdf92
ArockiaCarmelRaj <97453295+ArockiaCarmelRaj@users.noreply.github.com>
2022-01-12 16:02:41 +0530

    image at center

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import img1 from '../src/img/img.JPG'
 function App() {
   return (
-    <div> </div>
+    <div> 
+      
+    <center>
+    <img src={img1} style={{width:'200px', borderRadius
+    :'50%'}}/>
+
+    </center>
+
+    </div>
   );
 }
 
