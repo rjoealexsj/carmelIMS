9977aeaa08a3e36b89e7f1fbf79a14f9e4c44c25
ArockiaCarmelRaj <97453295+ArockiaCarmelRaj@users.noreply.github.com>
2022-01-14 13:57:41 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-import img1 from '../src/img/img.JPG'
+import './app.css';
+import './css/bootstrapp.min.css';
+import IDNavbar from './components/home';
+import {Col, FormGroup, Input, label, Row} from 'reactstrap';
+
 function App() {
   return (
-    <div> 
-      
-    <center>
-    <img src={img1} style={{width:'200px', borderRadius
-    :'50%'}}/>
+   <div className='container' style={{paddingtop:'50px'}}>
+     <div className='text-center p-4 bg-primary'>
+       <h1 style={{color: 'white'}}> Vidyajyoti Jesuit Residence </h1>
+            </div>
+            <FormGroup style= {{overflow: "visible", paddingtop: '50px'}}>
+              <Row style ={{marginBottom: '20px'}}>
+                <col xs={3} lg={2}> 
+                <label for="username"> Name </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Name' value =""/>
+                </col>
+                <col xs={3} lg={2}>
+                <label for="username1"> Name </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Name' value =""/>
+                </col>
+              </Row>
 
-    </center>
+              <Row style={{marginBottom:'20px'}}>
+              <col xs={3} lg={2}> 
+                <label for="username"> Email </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Email' value =""/>
+                </col>
+                <col xs={3} lg={2}>
+                <label for="username1"> Phone Number </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Phone Number' value =""/>
+                </col>
+              </Row>
 
+              <Row style={{marginBottom:'20px'}}>
+              <col xs={3} lg={2}> 
+                <label for="province"> province </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your province' value =""/>
+                </col>
+                <col xs={3} lg={2}>
+                <label for="dob"> Date of Birth </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Date of Birth' value =""/>
+                </col>
+              </Row>
+
+              <Row style={{marginBottom:'20px'}}>
+              <col xs={3} lg={2}> 
+                <label for="blood"> Blood Group </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Blood Group' value =""/>
+                </col>
+                <col xs={3} lg={2}>
+                <label for="roomno"> Room No </label>
+                </col>
+                <col xs={9} lg={4}>
+                <input type= "text" placeholder= 'Enter Your Room No' value =""/>
+                </col>
+                
+              </Row>
+            </FormGroup>
+            <div className= 'text-center'>
+              <button className= 'bth' style= {{border:'2px solid blue'}}> Submit </button>
+            </div>
+  
+  
+}
     </div>
+
+
+
   );
 }
 
